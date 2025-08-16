54ab6728a4777f3681cea43067d6be867a825a73
Raj <raaz2507@gmail.com>
2025-08-17 03:53:47 +0530

    add manual dice opction

--- js/script.js
+++ js/script.js
@@ -1,49 +1,196 @@
 class gameDashbord{
     #elemts={};
+    #players={};
     #BoxCenterList={};
     // #snakeLaddersPositionData ={ snakes : { 98: 66, 62: 19, 25: 5 }, ladders : { 4: 14, 9: 31, 40: 59 } }
     #snakeLaddersPositionData ={
         snakes: [
-            { start: 98, end: 66, image: './img/snake.png' },
-            { start: 62, end: 19, image: './img/snake.png' },
-            { start: 25, end: 5,  image: './img/snake.png' }
+            { start: 98, end: 27, image: './img/snake.png' },
+            { start: 62, end: 46, image: './img/snake.png' },
+            { start: 25, end: 7,  image: './img/snake.png' },
         ],
         ladders: [
-            { start: 4,  end: 14, image: './img/Ladder.png' },
+            { start: 4,  end: 22, image: './img/Ladder.png' },
             { start: 9,  end: 31, image: './img/Ladder.png' },
-            { start: 40, end: 59, image: './img/Ladder.png' }
+            { start: 40, end: 59, image: './img/Ladder.png' },
+            { start: 48, end: 89, image: './img/Ladder.png' },
         ]
     };
+    #lastDiceFace =-1;
+
     constructor(){
         this.#getElements();
         this.#createBord();
         this.#setEvents();
         // this.#getBoxCenter();
         this.#loadSnaksLadders_onBord();
+        this.#CreatePlayer();
     }
 
     #getElements(){
         this.#elemts['bord'] = document.getElementById('bord');
-        this.#elemts['dice'] = document.getElementById("dice"); 
+        this.#elemts['dice'] = document.getElementById("dice");
+
+        this.#elemts['toggleScreenBtn'] = document.getElementById('toggleScreenBtn');
+        this.#elemts['toggleBtnImg'] = document.getElementById('toggleScreenBtnImg');
+
+        this.#elemts['manualDiceRoll'] = document.getElementById('manualDiceRoll');
+
+        this.#elemts['diceToggleBtn'] = document.getElementById('diceToggleBtn');
     }
-    #getBoxCenter(){
-        const { bord }= this.#elemts;
-        
-        const boxs=  bord.querySelectorAll(':scope > div');
-
-        boxs.forEach((box)=>{
-            const rect =  box.getBoundingClientRect();
-            const X= Math.floor(rect.left + (rect.width/2)) + window.scrollX;
-            const Y= Math.floor(rect.top + (rect.height/2)) + window.scrollY;
-            
-            // console.log(`box=${box.dataset.box_num}, x= ${X} y=${Y}`);
-            this.#BoxCenterList[`${ box.dataset.box_num}`]= {x:X, y: Y};
-            // console.log(this.#BoxCenterList[`${ box.dataset.box_num}`]);
-
+    
+    
+    #setEvents(){
+        const { dice , toggleScreenBtn, manualDiceRoll, diceToggleBtn }=this.#elemts;
+        window.addEventListener('resize', () => {
+            document.querySelectorAll('.snakeImg').forEach(el => el.remove()); // पुरानी images हटाओ
+            document.querySelectorAll('.leaderImg').forEach(el => el.remove());
+            this.#loadSnaksLadders_onBord();
         });
-
-        // console.log(this.#BoxCenterList);
+        dice.addEventListener("click",()=>this.#rollDice() );
+        
+        toggleScreenBtn.addEventListener('click', ()=> this.#toggleScreen() );
+        
+        manualDiceRoll.addEventListener('click', (event)=>{
+            if (event.target.tagName === 'BUTTON'){
+                this.#lastDiceFace = event.target.value;
+            }    
+        });
+        
+        diceToggleBtn.addEventListener('change', (event)=>{
+            let diceTb = event.target.checked;
+            console.log(diceTb);
+            if (diceTb){
+                manualDiceRoll.classList.add('hide');
+                dice.classList.remove('hide');
+                document.getElementById('diceInstructions').innerText= "Tap to Roll Dice";
+            }else{
+                manualDiceRoll.classList.remove('hide');
+                dice.classList.add('hide');
+                document.getElementById('diceInstructions').innerText= "Select Dice Number";
+            }
+        });
     }
+    
+    #CreatePlayer(){
+        const {bord} =this.#elemts;
+        this.#players['1']={canvas: null, pos:{x:0, y:0}, charecter:{img: './player1.png'}};
+        
+        this.#players['1'].canvas=createPlayerObj();
+
+        console.log(this.#players[1]);
+        trycanvs(this.#players['1'].canvas);
+        function trycanvs(canvas){
+            const ctx = canvas.getContext('2d');
+            // कुछ ड्रॉ करके देखें
+            ctx.fillStyle = '#4caf50';
+            ctx.fillRect(50, 50, 150, 100);
+
+            ctx.beginPath();
+            ctx.arc(320, 200, 60, 0, Math.PI * 2);
+            ctx.fillStyle = '#ff7043';
+            ctx.fill();
+        }
+        function createPlayerObj(){
+            const canvas = document.createElement('canvas');
+            canvas.width= 100;
+            canvas.height=100;
+            canvas.style.position = 'absolute';
+            canvas.style.bottom = 20+ 'px';
+            canvas.style.left = 20 + 'px';
+
+            canvas.classList.add('player');
+            bord.appendChild(canvas);
+            return canvas;
+        }
+    }
+    #toggleScreen() {
+        const {toggleBtnImg} = this.#elemts;
+
+        const header = document.getElementsByTagName('header')[0];
+        const footer = document.getElementsByTagName('footer')[0];
+        if (header.classList.contains('hide')){
+            header.classList.remove('hide');
+            footer.classList.remove('hide');
+            toggleBtnImg.src = "./img/expand-solid-full.svg";
+        }else{
+            header.classList.add('hide');
+            footer.classList.add('hide');
+            toggleBtnImg.src = "./img/compress-solid-full.svg";
+        }
+    }
+
+    #rollDice(){
+        const diceFaces = ["⚀","⚁","⚂","⚃","⚄","⚅"];
+        
+        let count = 0;
+        let speed = 80; // शुरू में तेज
+        const maxRolls = 15; // कितनी बार face बदलेगा
+
+        const interval = setInterval(() => {
+            let newIndex;
+            do {
+                newIndex = Math.floor(Math.random() * diceFaces.length);
+            } while (newIndex === this.#lastDiceFace);
+
+            this.#lastDiceFace = newIndex;
+            dice.textContent = diceFaces[newIndex];
+            dice.style.transform = `rotate(${count * 90}deg)`; // घूमने का illusion
+
+            count++;
+            speed += 10; // धीरे-धीरे slow down
+
+            if (count >= maxRolls) {
+                clearInterval(interval);
+            }
+        }, speed);
+        
+    }
+
+    #createBord(){
+        const {bord}=this.#elemts;
+        const {snakes: snakeList, ladders: laddersList}= this.#snakeLaddersPositionData;
+        
+        let snakes = {};
+        let ladders = {};
+        snakeList.forEach( obj =>{ snakes[obj.start]= obj.end; });
+        laddersList.forEach( obj =>{ ladders[obj.start]= obj.end; });
+        
+        const boxfeg = document.createDocumentFragment();
+        for (let row = 9; row >= 0; row--) {
+            for (let col = 0; col < 10; col++) {
+                let number;
+                if (row % 2 === 0) {
+                    number = row * 10 + col + 1; // Left to Right
+                } else {
+                    number = row * 10 + (9 - col) + 1; // Right to Left
+                }
+
+                addBox(number);
+            }
+        }
+        bord.append(boxfeg);
+        
+
+        function addBox(num){
+            const newbox= document.createElement('div');
+            newbox.classList.add('box');
+            newbox.innerText = num;
+            newbox.dataset.box_num = num;
+            if (snakes[num]) {
+                newbox.classList.add('snake');
+                newbox.innerHTML += ' 🐍';
+                newbox.setAttribute('data-info', `To ${snakes[num]}`);
+            }
+            if (ladders[num]) {
+                newbox.classList.add('ladder');
+                newbox.innerHTML += ' 🪜';
+                newbox.setAttribute('data-info', `To ${ladders[num]}`);
+            }
+            boxfeg.appendChild(newbox);
+        }
+    }
+
     #loadSnaksLadders_onBord(){
         const {bord} =  this.#elemts;
         const {snakes, ladders}= this.#snakeLaddersPositionData;
@@ -91,78 +238,25 @@ class gameDashbord{
             image.style.left = `${offsetX}px`;
             image.style.transform = `rotate(${angle}deg)`;
         }
-
-
     }
-    #setEvents(){
-        const { dice }=this.#elemts;
-        window.addEventListener('resize', () => {
-            document.querySelectorAll('.snakeImg').forEach(el => el.remove()); // पुरानी images हटाओ
-            document.querySelectorAll('.leaderImg').forEach(el => el.remove());
-            this.#loadSnaksLadders_onBord();
+
+    #getBoxCenter(){
+        const { bord }= this.#elemts;
+        
+        const boxs=  bord.querySelectorAll(':scope > div');
+
+        boxs.forEach((box)=>{
+            const rect =  box.getBoundingClientRect();
+            const X= Math.floor(rect.left + (rect.width/2)) + window.scrollX;
+            const Y= Math.floor(rect.top + (rect.height/2)) + window.scrollY;
+            
+            // console.log(`box=${box.dataset.box_num}, x= ${X} y=${Y}`);
+            this.#BoxCenterList[`${ box.dataset.box_num}`]= {x:X, y: Y};
+            // console.log(this.#BoxCenterList[`${ box.dataset.box_num}`]);
+
         });
-        dice.addEventListener("click",rollDice);
-        
-        function rollDice () {
-            // 🎲 1 से 6 तक random number
-            const roll = Math.floor(Math.random() * 6) + 1;
 
-            // 🎯 Dice face update करें
-            dice.innerText = roll;
-
-            // 🌀 थोड़ा सा animation
-            dice.style.transform = "rotate(360deg) scale(1.2)";
-            setTimeout(() => {
-                dice.style.transform = "rotate(0deg) scale(1)";
-            }, 200);
-
-            // 🔊 Future use: player move, switch, etc.
-            console.log("Dice Rolled:", roll);
-        }
-    }
-    #createBord(){
-        const {bord}=this.#elemts;
-        const {snakes: snakeList, ladders: laddersList}= this.#snakeLaddersPositionData;
-        
-        let snakes = {};
-        let ladders = {};
-        snakeList.forEach( obj =>{ snakes[obj.start]= obj.end; });
-        laddersList.forEach( obj =>{ ladders[obj.start]= obj.end; });
-        
-        const boxfeg = document.createDocumentFragment();
-        for (let row = 9; row >= 0; row--) {
-            for (let col = 0; col < 10; col++) {
-                let number;
-                if (row % 2 === 0) {
-                    number = row * 10 + col + 1; // Left to Right
-                } else {
-                    number = row * 10 + (9 - col) + 1; // Right to Left
-                }
-
-                addBox(number);
-            }
-        }
-        bord.append(boxfeg);
-        
-
-        function addBox(num){
-            const newbox= document.createElement('div');
-            newbox.classList.add('box');
-            newbox.innerText = num;
-            newbox.dataset.box_num = num;
-            if (snakes[num]) {
-                newbox.classList.add('snake');
-                newbox.innerHTML += ' 🐍';
-                newbox.setAttribute('data-info', `To ${snakes[num]}`);
-            }
-            if (ladders[num]) {
-                newbox.classList.add('ladder');
-                newbox.innerHTML += ' 🪜';
-                newbox.setAttribute('data-info', `To ${ladders[num]}`);
-            }
-            boxfeg.appendChild(newbox);
-        }
-        
+        // console.log(this.#BoxCenterList);
     }
   
 }
